b400321dd926dd3efa1ac67a7ec9767242fa1457
Izaan <izaanqaiser05@gmail.com>
2024-03-11 16:32:59 -0400

    server-side configuration + adding express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000; 
 
 app.get("/", (req, res) => {
     // root route http:://localhost:5000/
     res.send("Hello World, today!");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/api/auth/signup")
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
