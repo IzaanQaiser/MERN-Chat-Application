e74876d836b314ef91a406f088e839169f4ee304
Izaan <izaanqaiser05@gmail.com>
2024-03-11 12:27:47 -0400

    tweaking some server things

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-const express = require("express")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    // root route http:://localhost:5000/
+    res.send("Hello World");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
