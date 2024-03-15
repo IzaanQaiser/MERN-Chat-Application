a4a8ea0284aa9aca8ba058c93a0df29598e404ec
Izaan <izaanqaiser05@gmail.com>
2024-03-15 11:14:58 -0400

    change name

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
-import connectToMongoDB from "./db/connectToMogoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
 dotenv.config();
