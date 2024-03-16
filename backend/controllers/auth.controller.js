2ef4017d24f83544a0291755a002c7f0a382feb4
Izaan <izaanqaiser05@gmail.com>
2024-03-16 18:03:20 -0400

    Configuring new profile creation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,45 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
     try {
         const {fullName, username, password, confirmPassword, gender} = req.body;
-    } catch (error){
 
+        if(password !== confirmPassword){
+          return res.status(400).json({error:"Passwords don't match"})
+        }
+
+      const user = await User.findOne({username});
+
+      if(user){
+        return res.status(400).json({error: "Username already exists"})
+      }
+
+      // HASHING THE PASSWORD
+
+      //Profile Picture API: https://avatar-placeholder.iran.liara.run/
+
+      const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+      const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+      const newUser = await User({
+        fullName,
+        username,
+        password,
+        gender,
+        profilePic: gender == "male" ? boyProfilePic : girlProfilePic
+      })
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser_id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic
+      })
+
+    } catch (error){
+      res.status(500).json({error: ""})
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -2,36 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
-
-dotenv.config();
-
 const PORT = process.env.PORT || 5000; 
 
-app.get("/", (req, res) => {
-    // root route http:://localhost:5000/
-    res.send("Hello World, today!");
-});
+dotenv.config();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-// Doing all the separate authentication routes is messy and will get cluttered so we will condense them into categories sorted by their root directory
-app.use("/api/auth", authRoutes) // Auth routes are called from the auth routes file whenever this path is followed
 
-// // 3 Authentication routes
-// app.get("/api/auth/signup", (req, res) => {
-//     console.log("signup route");
+// app.get("/", (req, res) => {
+//     // root route http:://localhost:5000/
+//     res.send("Hello World, today!");
 // });
 
-// app.get("/api/auth/login", (req, res) => {
-//     console.log("login route");
-// });
 
-// app.get("/api/auth/logout", (req, res) => {
-//     console.log("logout route");
-// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
-}
-
-);
+});
