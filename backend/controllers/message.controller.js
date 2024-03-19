a15f5523f4d8ba51895c405fe066b3ac43794678
Izaan <izaanqaiser05@gmail.com>
2024-03-19 11:45:19 -0400

    Setup message API error debugging

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,12 @@
 export const sendMessage = async (req, res) => {
-  console.log("message sent", req.params.id);
+  try {
+    const {message} = req.body;
+    const {id} = req.params;
+
+    const senderId = 0
+
+  } catch (error) {
+    console.log("Error in sendMessage Controller: ", error.message)
+    res.status(500).json({error: "Internal Server Error" })
+  }
 };
