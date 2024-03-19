e0b3417b6bd758c6fd24844b8aab04ab159334ac
Izaan <izaanqaiser05@gmail.com>
2024-03-19 09:49:48 -0400

    Complete all authentication routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,9 +83,10 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (req, res) => {
+export const logout = (req, res) => {
   try {
-    
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
