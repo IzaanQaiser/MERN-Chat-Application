4bdb123c0a7e2219f6c8741ad7b08295afac5f63
Izaan <izaanqaiser05@gmail.com>
2024-03-19 11:19:53 -0400

    Setup message sending route + function call

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 // app.get("/", (req, res) => {
