691876be6fa99b96978d68735ef9181b75c0c904
Izaan <izaanqaiser05@gmail.com>
2024-03-23 18:28:32 -0400

    Error handling

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -49,7 +49,11 @@ export const getMessages = async (req, res) => {
       participants: { $all: [senderId, userToChatId] },
     }).populate("messages");
 
-    res.status(200).json(conversation.messages);
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal Server Errror" });
