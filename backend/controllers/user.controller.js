4caff1e9f8723c9ce8edd5a771287069b0eea25a
Izaan <izaanqaiser05@gmail.com>
2024-03-25 19:07:13 -0400

    Update

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 // app.get("/", (req, res) => {
