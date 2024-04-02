2fd4ce378f9cd0226a44ba3115aaa6b12bfe701b
Izaan <izaanqaiser05@gmail.com>
2024-04-02 00:24:53 -0400

    Filtered Users list retrival

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,13 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
-    }
-} 
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } });
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
