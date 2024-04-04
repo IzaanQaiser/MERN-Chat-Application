fc2e255c7f51fe663ff349062690937617adb451
Izaan <izaanqaiser05@gmail.com>
2024-04-04 15:03:04 -0400

    setup rects and jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
   return;
-  <div className="p-4 h-screen flex items-center justify-center"></div>;
+  <div className="p-4 h-screen flex items-center justify-center">
+    <Login/>
+  </div>;
 }
 
 export default App;
