11f4943df93a00bc9d709219a726f92354b82af3
Izaan <izaanqaiser05@gmail.com>
2024-04-11 23:23:30 -0400

    signup page

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-    <div className="">
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="'w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <span className="text-blue-500"> ChatApp</span>
+        </h1>
 
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter full name"
+              className="w-full input input-bordered  h-10"
+            />
+          </div>
+
+          <div>
+            
+          </div>
+
+        </form>
+      </div>
     </div>
-
-  </div>;
+  );
 };
 
 export default SignUp;
